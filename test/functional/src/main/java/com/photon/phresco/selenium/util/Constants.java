c90fdfa42dbe7eff57a66d5475bef9eefd238c38
srividya <srividya.subramani@photoninfotech.net>
2012-06-26 18:43:55 +0530

    artf486232 committed fuctional folder

--- test/functional/src/main/java/com/photon/phresco/selenium/util/Constants.java
+++ test/functional/src/main/java/com/photon/phresco/selenium/util/Constants.java
@@ -1,22 +1,3 @@
-/*
- * ###
- * PHR_HTML5YUIWidget
- * %%
- * Copyright (C) 1999 - 2012 Photon Infotech Inc.
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ###
- */
 
 package com.photon.phresco.selenium.util;
 
@@ -41,6 +22,12 @@ public class Constants {
 	 * Execute tests on safari browser
 	 */
 	public static final String BROWSER_SAFARI = "*safari";
+	
+	/**
+	 * Execute tests on opera browser
+	 */
+	public static final String BROWSER_OPERA = "*opera";
+
 
 	public static String DEFAULT_TIMEOUT = "180000";
 
