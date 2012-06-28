7c5639efb930fa8b21c7a395eb6a63926df3e2bf
ganeshkumar-s <ganeshkumar.sivalingam@photoninfotech.net>
2012-06-28 19:10:39 +0530

    [artf486512] Implemented code changes corresponding to moving the phresco-env-config.xml from resources to WEB-INF/resources

--- src/main/java/com/photon/phresco/configuration/ConfigReader.java
+++ src/main/java/com/photon/phresco/configuration/ConfigReader.java
@@ -1,3 +1,22 @@
+/*
+ * ###
+ * PHR_HTML5YUIWidget
+ * %%
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */
 package com.photon.phresco.configuration;
 
 import java.io.File;
@@ -129,6 +148,26 @@ public class ConfigReader {
 			}
 		}
 		return props;
+	}
+	
+	public File getConfigFile() {
+		return configFile;
+	}
+	
+	public String getConfigAsJSON(String envName, String configType) {
+		if (envName == null) {
+			envName = getDefaultEnvName();
+		}
+		List<Configuration> configurations = getConfigByEnv(envName);
+		String json = "";
+		for (Configuration configuration : configurations) {
+			if (configuration.getType().equalsIgnoreCase(configType)) {
+				com.google.gson.Gson gson = new com.google.gson.Gson();
+				Properties properties = configuration.getProperties();
+				json = gson.toJson(properties, Properties.class);
+			}
+		}
+		return json;	
 	}
 
 }
