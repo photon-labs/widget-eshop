060be51fe5fe7bddf36e5041642b04f98ce7d809
dorothy-j <dorothy.judy@photoninfotech.net>
2012-07-16 16:15:39 +0530

    Modified pom.xml and UIConstants files

--- test/functional/src/test/java/com/photon/phresco/testcases/MobilePhonesAddcart.java
+++ test/functional/src/test/java/com/photon/phresco/testcases/MobilePhonesAddcart.java
@@ -7,14 +7,14 @@ import com.photon.phresco.Screens.MenuScreen;
 import com.photon.phresco.Screens.WelcomeScreen;
 import com.photon.phresco.uiconstants.PhrescoUiConstants;
 import com.photon.phresco.uiconstants.UIConstants;
-import com.photon.phresco.uiconstants.YUIWidgetData;
+import com.photon.phresco.uiconstants.WidgetData;
 import com.thoughtworks.selenium.Selenium;
 
 public class MobilePhonesAddcart extends TestCase {
 
 	private UIConstants phrsc;
 	private PhrescoUiConstants phr;
-	private YUIWidgetData yuiwidg;
+	private WidgetData yuiwidg;
 	private Selenium selenium;
 	private int SELENIUM_PORT;
 	private String browserAppends;
@@ -27,7 +27,7 @@ public class MobilePhonesAddcart extends TestCase {
 		try {
 
 			phrsc = new UIConstants();
-			yuiwidg = new YUIWidgetData();
+			yuiwidg = new WidgetData();
 			String serverURL = phr.PROTOCOL + "://" + phr.HOST + ":"
 					+ phr.PORT + "/";
 			browserAppends = "*" + phr.BROWSER;
