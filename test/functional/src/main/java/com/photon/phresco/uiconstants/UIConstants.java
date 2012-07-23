0685dff92f65b3c569f342db2d976db97a20e7f2
pradeep <pradeep.siluveru@photoninfotech.net>
2012-07-23 13:43:16 +0530

    put the commented for unused variable //public String PROD1_ACCESSORIES="prod1Accessories";

--- test/functional/src/main/java/com/photon/phresco/uiconstants/UIConstants.java
+++ test/functional/src/main/java/com/photon/phresco/uiconstants/UIConstants.java
@@ -23,6 +23,7 @@ public class UIConstants {
 	public String HOME = "hometab";
 	public String PROD1_DETAILS="prod1Details";
 	//public String PROD1_AUDIO="prod1AudioDevices";
+	
 //	public String PROD1_ACCESSORIES="prod1Accessories";
 	public String UPDATECART="updateCart";
 	public String ADDTOCART="addToCart"; 
